55a8c59dd90ef10e11bbb502929946840fed1610
ycl818 <kevinyichienlee@gmail.com>
2022-01-20 00:48:32 -0500

    ADD: map out the search results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -21,11 +21,13 @@ export const Results = () => {
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
           {results?.results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
-              <a href={link} target="_blank" ref="noreferrer">
+              <a href={link} target="_blank">
                 <p className="text-sm">
                   {link.length > 30 ? link.substring(0, 30) : link}
                 </p>
-                <p className="text-lg"></p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
               </a>
             </div>
           ))}
