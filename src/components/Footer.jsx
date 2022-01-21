110078a09fff653a7ccaa40ee9079123bf7eb8a5
ycl818 <kevinyichienlee@gmail.com>
2022-01-21 01:15:51 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
-      <h1>2022 Google</h1>
+      <h1>2022 Gooogle 😊 </h1>
     </div>
   );
 };
