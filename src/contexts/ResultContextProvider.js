6bbe057324ba819eabb472697dfe7986361838e0
ycl818 <kevinyichienlee@gmail.com>
2022-01-21 01:01:28 -0500

    hide env variable

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -18,7 +18,7 @@ export const ResultContextProvider = ({ children }) => {
         "x-user-agent": "desktop",
         "x-proxy-location": "US",
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": "7a67f5116bmsh18390da1a98dcdfp1dc430jsn9c9814ba865a",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
 
